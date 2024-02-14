45d897186bd0c01ec6a3627864d8c216c0d1f621
Erozak <pinex666@gmail.com>
2024-02-14 20:20:19 +0800

    feat: :sparkles: rewire with react qr code

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ export default function Index() {
   return (
     <main className="container py-16">
       <div className="mb-12 text-center">
-        <h1 className="text-3xl font-bold mb-4">Howazin</h1>
-        <p className="text-xl">
+        <h1 className="text-6xl font-bold mb-4">Howazin</h1>
+        <p className="text-l font-semibold">
           Generate QR code and share it with your friends to make they pay you
           easily.
           <br />
@@ -49,20 +49,17 @@ export default function Index() {
         {values && (
           <Card className="w-[385px]">
             <CardContent>
-              <div className="w-[285px] h-[285px] flex justify-center items-center mx-auto">
-                <TransferQRCodeImage
-                  className="block w-full h-auto"
-                  margin={1}
-                  width={280}
-                  height={280}
-                  payload={{
-                    accountNo: values.accountNo,
-                    amount: values.amount,
-                    bankCode: values.bankCode,
-                    message: values.note,
-                  }}
-                />
-              </div>
+              <TransferQRCodeImage
+                className="mx-auto"
+                size={180}
+                level="L"
+                payload={{
+                  accountNo: values.accountNo,
+                  amount: values.amount,
+                  bankCode: values.bankCode,
+                  message: values.note,
+                }}
+              />
             </CardContent>
             <CardFooter className="flex justify-start">
               <Button variant="outline">Back</Button>
--- app/transfer-qr-code-image.tsx
+++ app/transfer-qr-code-image.tsx
@@ -1,64 +1,28 @@
 'use client';
 
-import type { DetailedHTMLProps, ImgHTMLAttributes } from 'react';
+import QRCode, { QRCodeProps } from 'react-qr-code';
 
 import type { QRCodePayload } from '@twpay-qrcode/models/qr-code-payload/schema';
 import { fromQRCodePayloadToURL } from '@twpay-qrcode/models/qr-code-payload/to-url';
 
 export interface TransferQRCodeImageProps
-  extends Omit<
-    DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>,
-    'src' | 'alt'
-  > {
+  extends Omit<QRCodeProps, 'value' | 'ref'> {
   payload: QRCodePayload;
-  /**
-   * @range [99,547]
-   */
-  size?: number;
-  /**
-   * @default L
-   */
-  errorCorrectionLevel?: 'L' | 'M' | 'Q' | 'H';
-  /**
-   * @default 1
-   */
-  margin?: number;
 }
 
 export function TransferQRCodeImage({
   payload,
-  size: derivedSize = 400,
-  margin: derivedMargin = 1,
-  errorCorrectionLevel = 'L',
-  ...imageProps
+  ...qrCodeProps
 }: TransferQRCodeImageProps) {
   const url = fromQRCodePayloadToURL(payload);
 
-  const alt = `QR code for transfering to account "${payload.bankCode}-${payload.accountNo}"`;
-
-  const size = clampSize(derivedSize);
-  const margin = clampMargin(derivedMargin);
-
-  const qrcodeUrl = new URL('https://chart.googleapis.com/chart');
-  qrcodeUrl.searchParams.set('cht', 'qr');
-  qrcodeUrl.searchParams.set('chs', `${size}x${size}`);
-  qrcodeUrl.searchParams.set(
-    'chl',
-    url
-      .toString()
-      // Some bank apps only support uppercase protocol
-      .toUpperCase()
+  return (
+    <QRCode
+      {...qrCodeProps}
+      value={url
+        .toString()
+        // Some bank apps only support uppercase protocol
+        .toUpperCase()}
+    />
   );
-  qrcodeUrl.searchParams.set('choe', 'UTF-8');
-  qrcodeUrl.searchParams.set('chld', `${errorCorrectionLevel}|${margin}`);
-
-  return <img {...imageProps} alt={alt} src={qrcodeUrl.toString()} />;
-}
-
-function clampSize(value: number): number {
-  return Math.max(99, Math.min(547, value));
-}
-
-function clampMargin(value: number): number {
-  return Math.max(1, value);
 }
