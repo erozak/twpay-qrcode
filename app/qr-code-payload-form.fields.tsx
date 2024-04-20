472d4c519c9f173e905251d38d7af98fba4ae5c9
Erozak <pinex666@gmail.com>
2024-04-20 20:58:36 +0800

    feat: :sparkles: add dataset for bank code field

--- app/qr-code-payload-form.fields.tsx
+++ app/qr-code-payload-form.fields.tsx
@@ -1,9 +1,26 @@
 'use client';
 
+import { useMemo } from 'react';
 import { useController } from 'react-hook-form';
+import {
+  IconCircleCheckFilled,
+  IconAlertCircleFilled,
+} from '@tabler/icons-react';
+import {
+  TextInput,
+  Combobox,
+  useCombobox,
+  NumberInput,
+  ScrollArea,
+  Popover,
+  Text,
+  Box,
+} from '@mantine/core';
 
-import { TextInput, Combobox, useCombobox, NumberInput } from '@mantine/core';
+import BANK_CODES from '@twpay-qrcode/generated/bank-codes.json';
+
 import type { QRCodePayloadFormControl } from './qr-code-payload-form';
+import { useDisclosure } from '@mantine/hooks';
 
 export function BankCodeField(props: { control?: QRCodePayloadFormControl }) {
   const length = 3;
@@ -17,16 +34,20 @@ export function BankCodeField(props: { control?: QRCodePayloadFormControl }) {
   });
   const combobox = useCombobox();
 
-  const knownCodes: string[] = [];
-
-  const shouldFilterOptions =
-    field.value && !knownCodes.some((item) => item === field.value);
-
-  const options = shouldFilterOptions
-    ? knownCodes.filter((item) =>
-        item.toLowerCase().includes(field.value.toLowerCase().trim()),
+  const options = field.value
+    ? BANK_CODES.filter((item) =>
+        item.code.includes(field.value.toLowerCase().trim()),
       )
-    : knownCodes;
+    : BANK_CODES;
+
+  const canFindActiveOption = field.value && field.value.length === 3;
+
+  const activeOption = (() => {
+    if (!canFindActiveOption) return null;
+    if (options.length !== 1) return null;
+
+    return options[0];
+  })();
 
   return (
     <Combobox
@@ -44,32 +65,96 @@ export function BankCodeField(props: { control?: QRCodePayloadFormControl }) {
           pattern="\d*"
           maxLength={length}
           minLength={length}
-          value={field.value}
+          value={field.value || ''}
+          rightSection={
+            <BankCodeRecognizedIndicator
+              disabled={!canFindActiveOption}
+              activeItem={activeOption}
+            />
+          }
           error={fieldState.error?.message}
           onChange={(event) => {
             field.onChange(event);
             combobox.openDropdown();
             combobox.updateSelectedOptionIndex();
           }}
-          onClick={() => combobox.openDropdown()}
-          onFocus={() => combobox.openDropdown()}
-          onBlur={() => combobox.closeDropdown()}
+          onClick={() => {
+            combobox.openDropdown();
+          }}
+          onFocus={() => {
+            combobox.openDropdown();
+          }}
+          onBlur={() => {
+            combobox.closeDropdown();
+          }}
+          inputMode="numeric"
         />
       </Combobox.Target>
       <Combobox.Dropdown>
         <Combobox.Options>
-          {options.length === 0 ? (
-            <Combobox.Empty>Nothing found</Combobox.Empty>
-          ) : (
-            options.map((option) => (
-              <Combobox.Option value={option} key={option}>
-                {option}
-              </Combobox.Option>
-            ))
-          )}
+          <ScrollArea.Autosize type="scroll" mah={240}>
+            {options.length === 0 ? (
+              <Combobox.Empty>Nothing found</Combobox.Empty>
+            ) : (
+              options.map((option) => (
+                <Combobox.Option value={option.code} key={option.code}>
+                  {option.code} - {option.name}
+                </Combobox.Option>
+              ))
+            )}
+          </ScrollArea.Autosize>
         </Combobox.Options>
       </Combobox.Dropdown>
     </Combobox>
+  );
+}
+
+function BankCodeRecognizedIndicator(props: {
+  activeItem: (typeof BANK_CODES)[number] | null;
+  disabled: boolean;
+}) {
+  const { activeItem, disabled } = props;
+
+  const [opened, { close, open }] = useDisclosure(false);
+
+  if (disabled) return null;
+
+  return (
+    <Popover width={200} withArrow shadow="md" opened={opened}>
+      <Popover.Target>
+        <Box
+          c={activeItem ? 'green' : 'yellow'}
+          onMouseEnter={open}
+          onMouseLeave={close}
+          renderRoot={(rootProps) =>
+            activeItem ? (
+              <IconCircleCheckFilled {...rootProps} />
+            ) : (
+              <IconAlertCircleFilled {...rootProps} />
+            )
+          }
+        />
+      </Popover.Target>
+      <Popover.Dropdown>
+        <Text size="sm" c="dimmed">
+          {activeItem ? (
+            <>
+              Recognized as{' '}
+              <Text
+                span
+                fw={700}
+                style={{ color: 'var(--mantine-color-text)' }}
+              >
+                {activeItem.name}
+              </Text>
+              .
+            </>
+          ) : (
+            'Unrecognized.'
+          )}
+        </Text>
+      </Popover.Dropdown>
+    </Popover>
   );
 }
 
@@ -93,7 +178,7 @@ export function AccountNumberField(props: {
       pattern="\d*"
       minLength={rules.minLength}
       maxLength={rules.maxLength}
-      value={field.value}
+      value={field.value || ''}
       onChange={field.onChange}
       error={fieldState.error?.message}
     />
@@ -117,7 +202,7 @@ export function AmountField(props: { control?: QRCodePayloadFormControl }) {
       placeholder="000,000"
       min={rules.min}
       max={rules.max}
-      value={field.value}
+      value={field.value || ''}
       onChange={field.onChange}
       error={fieldState.error?.message}
     />
@@ -138,7 +223,7 @@ export function MessageField(props: { control?: QRCodePayloadFormControl }) {
       name="message"
       label="Message"
       placeholder="A message for the recipient"
-      value={field.value}
+      value={field.value || ''}
       onChange={field.onChange}
       error={fieldState.error?.message}
       maxLength={rules.maxLength}
